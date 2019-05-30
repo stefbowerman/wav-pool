5cad890e1b553d3afd991dc9a286b122ea8d3ebf
Stefan Bowerman <stefbowerman@gmail.com>
2019-05-30 11:21:41 -0700

    Homepage scaffold

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -24,6 +24,10 @@ module.exports = {
     ]),
     new HtmlWebpackPlugin({
       template: Path.resolve(__dirname, '../src/index.html')
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'catalog.html',
+      template: Path.resolve(__dirname, '../src/catalog.html')
     })
   ],
   resolve: {
